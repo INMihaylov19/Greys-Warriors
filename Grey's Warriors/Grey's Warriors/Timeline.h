ed459a326477017671e9aba2c79d6a1601ee9fa6
Veselin Stoqnov <VNStoyanov19@codingburgas.bg>
2022-02-03 18:21:02 +0200

    Add all functions to Timeline.h

--- Grey's Warriors/Grey's Warriors/Timeline.h
+++ Grey's Warriors/Grey's Warriors/Timeline.h
@@ -1,1 +1,31 @@
-#pragma once
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <string>
+#include <vector>
+#include "Menu.h"
+using namespace std;
+
+struct EventName
+{
+	string title;
+	EventName* next;
+};
+
+struct EventYear
+{
+	int date;
+	EventYear* next = NULL;
+};
+
+void drawTimelines();
+
+void newElementName(EventName* Head, string value);
+
+void newElementYear(EventYear* Head, int value);
+
+void addEventToNote(int repeatment, EventName* usedN, EventYear* usedY);
+
+void startNewNote(int repeat);
+
+void timelineSection();
