9b5af7e64e6f08c4417240a46f62edaf21cebb40
Veselin Stoqnov <VNStoyanov19@codingburgas.bg>
2022-02-03 08:50:07 +0200

    Create a function for drawing the content

--- Grey's Warriors/Grey's Warriors/Timeline.cpp
+++ Grey's Warriors/Grey's Warriors/Timeline.cpp
@@ -24,3 +24,42 @@ EventName** HeadN = new EventName * [10];
 EventYear** HeadY = new EventYear * [10];
 string TimeLineTitle[10];
 int coler = -1;
+
+
+void drawTimelines()
+{
+	system("cls");
+	int sizeCount;
+	for (int i = 0; i <= coler; ++i)
+	{
+		sizeCount = 0;
+		cout << TimeLineTitle[i];
+		for (size_t j = 0; j < (50 - TimeLineTitle[i].size()); j++) {
+			cout << ".";
+		}
+		EventName* temp = *(HeadN + i);
+		EventYear* tempY = *(HeadY + i);
+
+		//cout << i << "-->\t";
+		while (temp != NULL) {
+			//cout << temp->title << " ";
+			temp = temp->next;
+			sizeCount++;
+		}
+
+		/*cout << '\n' << i << "-->\t";
+		while (tempY != NULL) {
+			cout << tempY->date << " ";
+			tempY = tempY->next;
+		}*/
+
+		cout << sizeCount << endl;
+	}
+
+	cout << "\n\n\t Iskash li da se vurnesh nazad?";
+	char key;
+	key = _getch();
+	if (key == char(27)) {
+		timelineSection();
+	}
+}
