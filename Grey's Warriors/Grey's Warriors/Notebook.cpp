999d111ad95516d948d738ebfed26b09f4a4a4bb
INMihaylov19 <INMihaylov19@codingburgas.bg>
2022-02-10 00:37:38 +0200

    Add button in search section

--- Grey's Warriors/Grey's Warriors/Notebook.cpp
+++ Grey's Warriors/Grey's Warriors/Notebook.cpp
@@ -130,7 +130,20 @@ void searchBoxNotebook(int whichYear)
 		cout << " There is no result in Notebook\n\n";
 	}
 
-	cout << endl << "If you want to go back press ESC";
+	//Get back button
+	gotoxy(0, 10); cout << char(201);
+	for (int i = 0; i < 32; i++)
+	{
+		cout << char(205);
+	}
+	cout << char(187);
+	gotoxy(0, 11); cout << char(186) << "      Press Esc to get back     " << char(186) << endl;
+	gotoxy(0, 12); cout << char(200);
+	for (int i = 0; i < 32; i++)
+	{
+		cout << char(205);
+	}
+	cout << char(188);
 	do {
 		key = _getch();
 		if (key == char(27)) {
