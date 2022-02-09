a1ad99b06f206344fe3f95e53117d8d473bfd393
INMihaylov19 <INMihaylov19@codingburgas.bg>
2022-02-09 12:29:45 +0200

    Add SFML library and files

--- Grey's Warriors/Grey's Warriors/Main.cpp
+++ Grey's Warriors/Grey's Warriors/Main.cpp
@@ -1,3 +1,4 @@
+#include <SFML/Graphics.hpp>
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
@@ -7,5 +8,21 @@ using namespace std;
 
 int main()
 {
-    Menu();
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
