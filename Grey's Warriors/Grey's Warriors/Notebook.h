c66886303e85a7e39144d5a357012e5678e3343e
Ivaylo <IPMarkov19@codingburgas.bg>
2022-02-11 16:26:21 +0200

    Edit notebook.h

--- Grey's Warriors/Grey's Warriors/Notebook.h
+++ Grey's Warriors/Grey's Warriors/Notebook.h
@@ -1,19 +1,52 @@
 #pragma once
-
-#include <iostream>  
+#include <iostream>
 #include <windows.h>
 #include <conio.h>
-
-void art();
-
-void searchBoxNotebook(int whichYear);
-
-std::string extractInfoNotebook(int& i, std::string info, std::string line);
-
-void inputFromFileNotebook();
-
-void notebookCover();
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
 
 void mainGrid();
 
+void art();
+
+void textField(int size = 37);
+
+struct Title
+{
+
+	vector<string> event;
+	Title* next = NULL;
+};
+
+void newElement(Title* Head, vector<string> value);
+
+string extractInfoNotebook(int& i, std::string info, std::string line);
+
+void inputFromFileNotebook();
+
+void setDateToFileNotebook(vector<string> temp, bool del = false);
+
+
+void addEventToNotebook();
+
+void searchBoxNotebook(int whichYear);
+
+void del_pos(struct Title** Head1, int position);
+
+void drawPageContent(int pageNumber);
+
+void drawNotebookContent(bool isAdd, bool isDel, bool isOpen = false);
+
+void addEventToNotebook();
+
+void startNewNotebook();
+
+
+
+void notebookCover();
+
 int bookMenu();
+
+void mainGrid();
