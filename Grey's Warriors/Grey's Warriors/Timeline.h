ea9cf14cb718bf76df8c8df37f45b9194e989176
Ivaylo <IPMarkov19@codingburgas.bg>
2022-02-11 16:26:40 +0200

    Edit timeline.h

--- Grey's Warriors/Grey's Warriors/Timeline.h
+++ Grey's Warriors/Grey's Warriors/Timeline.h
@@ -1,28 +1,24 @@
+#pragma once
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
 #include <string>
+#include <fstream>
 #include <vector>
-#include "Menu.h"
-using namespace std;
+
+void timelineSection();
 
 struct EventName
 {
 	string title;
-	EventName* next;
+	EventName* next = NULL;
 };
 
 struct EventYear
 {
-	int date;
+	int date = 0;
 	EventYear* next = NULL;
 };
-
-void searchBoxTimeline(int whichYear);
-
-void Field(int size);
-
-void drawTimelines();
 
 void newElementName(EventName* Head, string value);
 
@@ -32,10 +28,22 @@ std::string extractInfo(int& i, std::string info, std::string line);
 
 void inputFromFileTimeline();
 
+void searchBoxTimeline(int whichYear);
+
+void Field(int size);
+
+void bubbleSort(int array[], string arrayS[], int size);
+
+void drawTimelineInfo(int numTimel);
+
+void drawTimelines();
+
+void setDateToFileTimeline(EventName*& tempN, EventYear*& tempY, bool newLine, string tTitle = " ", int size = 0);
+
 void addEventToNote(int repeatment, EventName* usedN, EventYear* usedY);
 
-void numberOfEvents();
-
 void startNewNote(int repeat);
+
+void numberOfEvents();
 
 void timelineSection();
