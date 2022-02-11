ed8e0e199373cebd9b38fcc84a8259b1d3cd6c31
Ivaylo <IPMarkov19@codingburgas.bg>
2022-02-11 16:25:52 +0200

    Edit menu.h

--- Grey's Warriors/Grey's Warriors/Menu.h
+++ Grey's Warriors/Grey's Warriors/Menu.h
@@ -1,14 +1,14 @@
 #pragma once
-
-#include <iostream>  
-#include <windows.h>
+#include <iostream>
 #include <conio.h>
+#include <windows.h>
 
-
-void color(int color);
+void color(int color); 
 
 void gotoxy(int x, int y);
 
 void menuArt(int SetColor[]);
+
+void searchEngine();
 
 int Menu();
