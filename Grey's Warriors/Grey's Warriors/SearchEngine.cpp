f27e7eaf21065ee2ac106ca9fb31b86d042c7ce5
Veselin Stoqnov <VNStoyanov19@codingburgas.bg>
2022-02-12 12:51:23 +0200

    Create a convertion from bin to gray

--- Grey's Warriors/Grey's Warriors/SearchEngine.cpp
+++ Grey's Warriors/Grey's Warriors/SearchEngine.cpp
@@ -6,6 +6,23 @@
 #include "Notebook.h"
 using namespace std;
 
+
+int convertSecond(int binary)
+{
+	int a, b, gray = 0, i = 0;
+	while (binary != 0)
+	{
+		a = binary % 10;
+		binary = binary / 10;
+		b = binary % 10;
+
+		if ((a && !b) || (!a && b))
+			gray = gray + pow(10, i);
+		i++;
+	}
+	return convertThird(gray);
+}
+
 int firstConvert(int n)
 {
 	int bin = 0;
